7d273b1971fc2f73fa7c66adf02f3558540efc36
nhbouman <n.h.bouman@student.tudelft.nl>
2018-01-11 12:33:48 +0100

    Pushed because changing branch

--- main.m
+++ main.m
@@ -1,29 +1,7 @@
-img = imread('images/10.JPG');
+img = imread('images/5.JPG');
 grayImage = rgb2gray(img);
 X = adjustedImage2(img);
 % A = cropToPlate(img,X);
-imshow(X)
 
-% measurements = regionprops(X, 'Area', 'BoundingBox');
-% allAreas = [measurements.Area]
-% % Crop out each word
-% for blob = 1 : length(measurements)
-% 	% Get the bounding box.
-% 	thisBoundingBox = measurements(blob).BoundingBox;
-% 	% Crop it out of the original gray scale image.
-% 	thisWord = imcrop(grayImage, thisBoundingBox);
-% 	imshow(thisWord); % Display it.
-% end
-% A = cropToPlate(img);
-% [B,C] = adjustedImage(img);
-% imshow(A)
-% figure
-% imshow(B)
-%C = imfuse(A,B)
-% D = C & 0;
-% for obj = 1 : length(B)
-%     if (B == obj)
-%     D = D | B == obj;
-% end
-% figure
-% imshow(D);
+B = borderAroundChar(X);
+imshow(B)
