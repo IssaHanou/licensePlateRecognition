1d2bdeb3746cec2bac5821d32c0d6871c7f64ca6
nhbouman <n.h.bouman@student.tudelft.nl>
2018-01-19 11:59:58 +0100

    Now crops only on the sides

--- processImage.m
+++ processImage.m
@@ -5,17 +5,29 @@ plate = getPlate(img); %Execute getPlate with the image
 croppedImage = getCroppedPlate(binaryImage, plate);
 img = rotImage(croppedImage);
 imshow(img)
-
-
+%[hist, bin] = diphist(img, [1, 250], 250);
+% figure;
+% imhist(img)
+% [x y] = imhist(img);
+% count = median(x);
+% count2 = mean(y);
+% disp(count)
+% disp(count2)
+% %figure;
+% %stem(bin, hist);
+% img(img >= 110) = 0;
+% img(img > 0) = 255;
+% figure;
+% imshow(img)
 % struct = regionprops(croppedImage , 'Area', 'BoundingBox'); 
 % [x maxArea] = max([struct.Area]);
 % I = imcrop(plate,struct(maxArea).BoundingBox);
 % imshow(I)
 
 
-
+% st = regionprops(croppedImage , 'Area', 'BoundingBox'); 
 % array = [st.BoundingBox]; %Get an array of all bounding boxes
-% imshow(labelImage);
+% imshow(img);
 % hold on;
 % for n=1:4:length(array) %Go through the array, but skip 4 as each 4 numbers are the corners of the bounding box
 %     if(array(n+3) > 2)
@@ -23,9 +35,9 @@ imshow(img)
 %     end
 % end
 % hold off;
-% im2 = imfill(labelImage);
+% im2 = imfill(img);
 % st2 = regionprops( im2, 'Area', 'BoundingBox' );
 % [a b] = max([st2.Area]);
-%figure;
-%imshow()
+% figure;
+% imshow()
 end
