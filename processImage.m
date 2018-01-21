8f82c0a7ba9c965d8e5d62b2228fff1649818a91
nhbouman <n.h.bouman@student.tudelft.nl>
2018-01-21 12:04:20 +0100

    Now works for all grayscale filters

--- processImage.m
+++ processImage.m
@@ -3,8 +3,7 @@ function processImage
 % dipimage;
 
 %Read the input image
-img = imread('images/1.JPG'); 
-
+img = imread('images/1.jpg'); 
 %Execute getPlate with the image
 plate = getPlate(img); 
 
@@ -13,12 +12,15 @@ plate = getPlate(img);
 
 %Execute getCroppedPlate with the plate image and binary image
 croppedImage = getCroppedPlate(binaryImage, plate);
+figure;
+imshow(croppedImage)
 
 %Execute rotImage with the cropped image
 img = rotImage(croppedImage);
-
+imshow(img)
 %Execute getAllLetters with the rotated image
 [a,b,c,d,e,f] = getAllLetters(img);
+figure;
 imshow(a)
 figure;
 imshow(b)
