6ac510328f2ab7084b4da5fbcbcb24a119813243
nhbouman <n.h.bouman@student.tudelft.nl>
2018-01-26 15:45:34 +0100

    Now logically changes numbers into letters and vice versa depending on the others

--- checkLicensePlate.m
+++ checkLicensePlate.m
@@ -1,36 +1,34 @@
-function license = checkLicensePlate(licensePlate, pos1, pos2)
+function licensePlate = checkLicensePlate(licensePlate, pos1, pos2)
 nums = getPossibleChars(2);
-numbers = 0;
-if and(pos1==3,pos2==7) || and(pos1==7,pos2==3)
-    %Do something
-elseif and(pos1==3,pos2==6) || and(pos1==6,pos2==3)
-    if contains(nums,licensePlate(1)) && contains(nums,licensePlate(2))
-        number1 = 1;
-        number2 = 2;
-    elseif contains(nums,licensePlate(3)) && contains(nums,licensePlate(4))
-        number1 = 4;
-        number2 = 5;
-    elseif contains(nums,licensePlate(5)) && contains(nums,licensePlate(6))
-        number1 = 7;
-        number2 = 8;
-    end
-    for i=1:8
-        if i ~= number1 && i~= number2
-            if contains(nums,licensePlate(i))
-                if licensePlate(i) == '8' || licensePlate(i) == '3'
-                    licensePlate(i) = 'B';
-                elseif licensePlate(i) == '1'
-                    licensePlate(i) = 'T'
-                elseif licensePlate(i) == '2'
-                    licensePlate(i) = 'Z'
-                elseif licensePlate(i) == '5'
-                    licensePlate(i) = 'J'
-                elseif licensePlate(i) == '9'
-                    licensePlate(i) = 'P' %or R
-                end
-            end
+number1 = 0;
+number2 = 0;
+number3 = 0;
+    if and(pos1==3,pos2==7) || and(pos1==7,pos2==3)
+        if and(contains(nums,licensePlate(1)), contains(nums,licensePlate(2))) || and(contains(nums,licensePlate(1)), contains(nums,licensePlate(8))) || and(contains(nums,licensePlate(8)), contains(nums,licensePlate(2)))
+            number1 = 1;
+            number2 = 2;
+            number3 = 8;
+        elseif and(contains(nums,licensePlate(4)), contains(nums,licensePlate(5))) && and(contains(nums,licensePlate(5)), contains(nums,licensePlate(6))) || and(contains(nums,licensePlate(4)), contains(nums,licensePlate(6)))
+            number1 = 4;
+            number2 = 5;
+            number3 = 6;
+        end
+        if number1 ~= 0 && number2 ~= 0 && number3 ~= 0
+            licensePlate = changeChar(licensePlate,number1,number2,number3);
+        end
+    elseif and(pos1==3,pos2==6) || and(pos1==6,pos2==3)
+        if contains(nums,licensePlate(1)) && contains(nums,licensePlate(2))
+            number1 = 1;
+            number2 = 2;
+        elseif contains(nums,licensePlate(4)) && contains(nums,licensePlate(5))
+            number1 = 4;
+            number2 = 5;
+        elseif contains(nums,licensePlate(7)) && contains(nums,licensePlate(8))
+            number1 = 7;
+            number2 = 8;
+        end
+        if number1 ~= 0 && number2 ~= 0
+            licensePlate = changeChar(licensePlate, number1, number2, number3);
         end
     end
-end
-license = licensePlate;
 end
