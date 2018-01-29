e1bcbc09f211eda7ef68c1ea377d166086e1c8af
Issa <i.k.hanou@student.tudelft.nl>
2018-01-29 17:07:03 +0100

    fixed checkTriples

--- checkTriples.m
+++ checkTriples.m
@@ -1,7 +1,14 @@
 function licensePlateString = checkTriples(licensePlateString)
-noStripes = erase(licensePlateString,'-');
+noStripes = erase(licensePlateString,'-');  
+arrayLicense = [];
 for i=1:6
-    if numel(find(noStripes(i))) > 2
+    arrayLicense(end+1) = licensePlateString(i);
+end
+uniques = unique(arrayLicense);
+count = hist(arrayLicense,uniques)
+
+for i=1:length(count)
+    if count(i) > 2
         licensePlateString = '';
         return;
     end
