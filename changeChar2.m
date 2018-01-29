ef8a9f2fe5ac8a869b18cca3881a3ee6ed5899ac
Issa <i.k.hanou@student.tudelft.nl>
2018-01-29 15:47:22 +0100

    deleted old code with many if statements

--- changeChar2.m
+++ changeChar2.m
@@ -5,99 +5,17 @@ for i=1:length(license)
     if i == number1 || i == number2 || i == number3
         if ~contains(nums,license(i))
             im = getIndexedImage(i,img1,img2,img3,img4,img5,img6,pos1,pos2);
-%             if and(pos1==3, pos2==6)
-%                 if i==1
-%                     im = img1;
-%                 elseif i == 2
-%                     im = img2;
-%                 elseif i == 4
-%                     im = img3;
-%                 elseif i == 5
-%                     im = img4;
-%                 elseif i == 7
-%                     im = img5;
-%                 elseif i == 8
-%                     im = img6;
-%                 end
-%             elseif and(pos1==3, pos2==7)
-%                 if i==1
-%                     im = img1;
-%                 elseif i == 2
-%                     im = img2;
-%                 elseif i == 4
-%                     im = img3;
-%                 elseif i == 5
-%                     im = img4;
-%                 elseif i == 6
-%                     im = img5;
-%                 elseif i == 8
-%                     im = img6;
-%                 end
-%             elseif and(pos1==2,pos2==6)
-%                 if i==1
-%                     im = img1;
-%                 elseif i == 3
-%                     im = img2;
-%                 elseif i == 4
-%                     im = img3;
-%                 elseif i == 5
-%                     im = img4;
-%                 elseif i == 7
-%                     im = img5;
-%                 elseif i == 8
-%                     im = img6;
-%                 end
-%             end
             if im ~= 0
                 license(i) = detectLetter(im, 2, true);
+                letter = license(i);
+                if contains(getPossibleChars(10),letter)
+                    license(i) = extraLetterCheck(letter,im);
+                end
             end
         end
     elseif i ~= pos1 || i ~= pos2
         if contains(nums,license(i))
             im = getIndexedImage(i,img1,img2,img3,img4,img5,img6,pos1,pos2);
-%             if and(pos1==3, pos2==6)
-%                 if i==1
-%                     im = img1;
-%                 elseif i == 2
-%                     im = img2;
-%                 elseif i == 4
-%                     im = img3;
-%                 elseif i == 5
-%                     im = img4;
-%                 elseif i == 7
-%                     im = img5;
-%                 elseif i == 8
-%                     im = img6;
-%                 end
-%             elseif and(pos1==3, pos2==7)
-%                 if i==1
-%                     im = img1;
-%                 elseif i == 2
-%                     im = img2;
-%                 elseif i == 4
-%                     im = img3;
-%                 elseif i == 5
-%                     im = img4;
-%                 elseif i == 6
-%                     im = img5;
-%                 elseif i == 8
-%                     im = img6;
-%                 end
-%             elseif and(pos1==2,pos2==6)
-%                 if i==1
-%                     im = img1;
-%                 elseif i == 3
-%                     im = img2;
-%                 elseif i == 4
-%                     im = img3;
-%                 elseif i == 5
-%                     im = img4;
-%                 elseif i == 7
-%                     im = img5;
-%                 elseif i == 8
-%                     im = img6;
-%                 end
-%             end
             if im ~= 0
                 license(i) = detectLetter(im, 1 ,true);
                 letter = license(i);
