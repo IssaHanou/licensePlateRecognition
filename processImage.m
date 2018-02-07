f825c4ce55ef029d31bfe91c38e25d81ce6b6e6f
nhbouman <n.h.bouman@student.tudelft.nl>
2018-02-07 11:13:53 +0100

    Now gets fully cropped to license plate.

--- processImage.m
+++ processImage.m
@@ -1,7 +1,19 @@
-image = imread('images/10.JPG');
+image = imread('images/13.JPG');
 %Resize the image (as it is too big)
 img = imresize(image,[400 NaN]);
 [colorPlate,binaryPlate] = getPlate(img);
-colorRot = rotateImg(colorPlate,binaryPlate);
+[colorRot,angle] = rotateImg(colorPlate,binaryPlate);
 figure;
 imshow(colorRot)
+[height,width,~] = size(colorRot);
+alpha = abs(angle); %The angle with which the image is rotated
+AC = sin(alpha*pi/180)*height; %Calculate the distance of the rotated image (black triangles)
+BD = sin(alpha*pi/180)*width;
+%Set coordinates
+x = AC;
+y = BD;
+w = width - 2*AC;
+h = height - 2*BD;
+onlyPlate = imcrop(colorRot,[x y w h]); %Crop to the plate
+figure;
+imshow(onlyPlate)
--- rotateImg.m
+++ rotateImg.m
@@ -1,4 +1,4 @@
-function colorRot = rotateImg(colorPlate, binaryPlate)
+function [colorRot,angle] = rotateImg(colorPlate, binaryPlate)
 struct = regionprops(binaryPlate , 'Orientation', 'BoundingBox'); 
 angle = struct.Orientation; % Get the angle of the license plate
 if angle ~= 90 % If the plate is already horizontal, don't rotate
