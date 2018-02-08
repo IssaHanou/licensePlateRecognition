d049c4ade1ddf708f672c0506549222535d8e0ec
Issa <i.k.hanou@student.tudelft.nl>
2018-02-08 14:48:19 +0100

    All hog features of the database are calculated in getAlphabet and then in detectLetter, only the current image calculate its hogs, then choose letter with smallest difference

--- processImage.m
+++ processImage.m
@@ -20,9 +20,21 @@ plateIm = cropToPlate(colorRot, angle);
 % figure
 % imshow(plateIm);
 plateString = 'AA-BB-33';
+
+
+%Get the average height and width from a letter picture
+width = 50;
+height = 100;
+%Get the hog features for all chars: m x n matrix: 
+%m = 36: first all numbers 0-9 then all letters A-Z
+%n = 1980: the length of the hog features array
+alphabet = getAlphabet(width,height);
+letter = detectLetter(img,alphabet,width,heigth);
+
 end
 
 %ideas: 
 %1)compute all images for alphabet once
 %2)not max(area) but > thres - getPlate for two license plates
 %3)boundingboxes, when number isn't > thres - stop, if higher > 6 foreign
+%4)FIRST LETTERS: B, F, G, H, J, K, L, N, P, R, S, T, V, X en Z.
