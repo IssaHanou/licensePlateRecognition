1dddde5a7b9bf6c92311424cf7696d3e45ce6f64
Issa <i.k.hanou@student.tudelft.nl>
2018-02-08 13:44:56 +0100

    Gets at least one frame with a plate per car. Changed the threshold values for yellow. And solution files are requested now

--- processImage.m
+++ processImage.m
@@ -5,12 +5,25 @@ img = imresize(image,[400 NaN]);
 %Get the plate out of the image
 [colorPlate,binaryPlate] = getPlate(img);
 
+%If no plate was found, return zeroes.
+if colorPlate == 0
+    plateIm = -1;
+    plateString = '';
+    return
+end
+
 %Rotate the plate to get it horizontal
 [colorRot,angle] = rotateImg(colorPlate,binaryPlate);
 
 %Crop the plate after rotation, depending on angle of rotation
 plateIm = cropToPlate(colorRot, angle);
-figure
-imshowpair(colorPlate, binaryPlate, 'montage');
+% figure
+% imshow(plateIm);
 plateString = 'AA-BB-33';
 end
+
+
+%ideas: 
+%1)compute all images for alphabet once
+%2)not max(area) but > thres - getPlate for two license plates
+%3)boundingboxes, when number isn't > thres - stop, if higher > 6 foreign
