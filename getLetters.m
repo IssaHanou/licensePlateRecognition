addcb991a18de4ea5ee389d537c6a35f0a372279
nhbouman <n.h.bouman@student.tudelft.nl>
2018-02-09 09:19:55 +0100

    No infinite loop anymore

--- getLetters.m
+++ getLetters.m
@@ -22,14 +22,15 @@ bw = imcomplement(bw);
 st = regionprops(bw, 'Area', 'BoundingBox'); 
 areas = [st.Area]; %Into array
 current = 1;
-while current ~= 7
+endAt = 0;
+while current ~= 7 && endAt ~= 15
     [~,n] = max( areas ); %Get the biggest area
     areas(n) = 0; %Set 0 for that position so we can get the second biggest area next time
     array = [st.BoundingBox]; %Get the coordinates of that area
     begin = n + (3*(n-1));
     z = 0;
     %If scale is ok, crop the image
-    if (array(begin+3)/array(begin+2)) > 1 && (array(begin+3)/array(begin+2)) < 3
+    if (array(begin+3)/array(begin+2)) > 1 & (array(begin+3)/array(begin+2)) < 3
         rect = [array(begin)+z array(begin+1)+z array(begin+2)+z array(begin+3)+z];
         %Save these coordinates
         xcoor(current) = array(begin);
@@ -40,54 +41,56 @@ while current ~= 7
         previous = array(begin);
         current = current + 1;
     end
+    endAt = endAt + 1;
 end
-
-%Get the images in the right order
-prevpos = 0;
-for p=1:6
-    [~,k] = min(xcoor);
-    if prevpos > 0
-        prev = ending(prevpos);
+if endAt ~= 15
+    %Get the images in the right order
+    prevpos = 0;
+    for p=1:6
+        [~,k] = min(xcoor);
+        if prevpos > 0
+            prev = ending(prevpos);
+        end
+        if p == 1
+            img1 = imcrop(img, [xcoor(k) ycoor(k) width(k) height(k)]);
+        elseif p == 2
+            img2 = imcrop(img, [xcoor(k) ycoor(k) width(k) height(k)]);
+            dist(1) = xcoor(k) - prev;
+        elseif p == 3
+            img3 = imcrop(img, [xcoor(k) ycoor(k) width(k) height(k)]);
+            dist(2) = xcoor(k) - prev;
+        elseif p == 4
+            img4 = imcrop(img, [xcoor(k) ycoor(k) width(k) height(k)]);
+            dist(3) = xcoor(k) - prev;
+        elseif p == 5
+            img5 = imcrop(img, [xcoor(k) ycoor(k) width(k) height(k)]);
+            dist(4) = xcoor(k) - prev;
+        elseif p == 6
+            img6 = imcrop(img, [xcoor(k) ycoor(k) width(k) height(k)]);
+            dist(5) = xcoor(k) - prev;
+        end
+        xcoor(k) = intmax;
+        prevpos = k;
     end
-    if p == 1
-        img1 = imcrop(img, [xcoor(k) ycoor(k) width(k) height(k)]);
-    elseif p == 2
-        img2 = imcrop(img, [xcoor(k) ycoor(k) width(k) height(k)]);
-        dist(1) = xcoor(k) - prev;
-    elseif p == 3
-        img3 = imcrop(img, [xcoor(k) ycoor(k) width(k) height(k)]);
-        dist(2) = xcoor(k) - prev;
-    elseif p == 4
-        img4 = imcrop(img, [xcoor(k) ycoor(k) width(k) height(k)]);
-        dist(3) = xcoor(k) - prev;
-    elseif p == 5
-        img5 = imcrop(img, [xcoor(k) ycoor(k) width(k) height(k)]);
-        dist(4) = xcoor(k) - prev;
-    elseif p == 6
-        img6 = imcrop(img, [xcoor(k) ycoor(k) width(k) height(k)]);
-        dist(5) = xcoor(k) - prev;
+
+    %Get the positions of the stripes
+    [~,temp1] = max(dist);
+    dist(temp1) = 0;
+    [~,temp2] = max(dist);
+    if temp1 > temp2
+        pos1 = temp2+1;
+        pos2 = temp1+2;
+    else
+        pos1 = temp1+1;
+        pos2 = temp2+2;
     end
-    xcoor(k) = intmax;
-    prevpos = k;
-end
 
-%Get the positions of the stripes
-[~,temp1] = max(dist);
-dist(temp1) = 0;
-[~,temp2] = max(dist);
-if temp1 > temp2
-    pos1 = temp2+1;
-    pos2 = temp1+2;
-else
-    pos1 = temp1+1;
-    pos2 = temp2+2;
-end
-
-%Make sure pos1 < pos2
-if pos1 > pos2
-    tmp = pos1;
-    pos1 = pos2;
-    pos2 = tmp;
+    %Make sure pos1 < pos2
+    if pos1 > pos2
+        tmp = pos1;
+        pos1 = pos2;
+        pos2 = tmp;
+    end
 end
 
 end
